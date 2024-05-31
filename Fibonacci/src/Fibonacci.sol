d66452ac05142d3be32c7bf08d846d241e4987e3
AlbertouSS <soy_uno_2008@hotmail.com>
2024-05-30 20:49:59 -0600

    Excercise Solved

--- Fibonacci/src/Fibonacci.sol
+++ Fibonacci/src/Fibonacci.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.13;
+import "forge-std/console.sol";
 
 contract Fibonacci {
     /*
@@ -11,7 +12,26 @@ contract Fibonacci {
         calling fibonacci(6) would return 8, because the 6th Fibonacci number is 8.
     */
 
-    function fibonacci(uint256 _position) public view returns (uint256) {
+    function fibonacci(uint256 _position) public pure returns (uint256) {
         // your code here
+            if (_position == 0) {
+            return 0;
+        } else if (_position == 1 || _position == 2) {
+            return 1;
+        } else {
+            uint256 m = 1;
+            uint256 n = 1;
+
+            for (uint256 i = 2; i < _position; i++) {
+                uint256 number = n + m;
+                n = m;
+                m = number;
+            }
+
+            return m;
+        }
     }
 }
+
+
+
