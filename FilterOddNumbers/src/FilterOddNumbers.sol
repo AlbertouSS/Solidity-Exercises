1f4c499459a0a16b3c46a33e81d8708925c875c0
AlbertouSS <soy_uno_2008@hotmail.com>
2024-06-10 20:59:21 -0600

    Excercise Solved

--- FilterOddNumbers/src/FilterOddNumbers.sol
+++ FilterOddNumbers/src/FilterOddNumbers.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.13;
+import "forge-std/console.sol";
 
 contract FilterOddNumbers {
     /*
@@ -10,11 +11,26 @@ contract FilterOddNumbers {
               you need to count the even numbers then declare an array of that size.
     */
 
-    function filterOdd(uint256[] memory _arr)
-        public
-        view
-        returns (uint256[] memory)
+    function filterOdd(uint256[] memory _arr) public pure returns (uint256[] memory)
     {
         // your code here
+        uint even_size = 0;
+        // get the number of even elements
+        for(uint i = 0; i < _arr.length; i++ ){
+            if(_arr[i] % 2 == 0){
+                even_size++;
+            }
+        }
+        // create a fixed size array in memory
+        uint[] memory even_array = new uint[](even_size);
+        //index of even elements
+        uint even_element = 0;
+        for(uint i = 0; i < _arr.length; i++ ){
+            if(_arr[i] % 2 == 0){
+                even_array[even_element] = _arr[i];
+                even_element++;
+            }
+        }
+        return even_array;
     }
 }
