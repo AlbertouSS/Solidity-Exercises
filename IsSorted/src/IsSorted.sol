91001441e0d693d6977bf75911d4418769cf39b4
AlbertouSS <soy_uno_2008@hotmail.com>
2024-06-18 19:25:54 -0600

    Excercise Solved

--- IsSorted/src/IsSorted.sol
+++ IsSorted/src/IsSorted.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.13;
+import "forge-std/console.sol";
 
 contract IsSorted {
     /**
@@ -7,5 +8,16 @@ contract IsSorted {
      */
     function isSorted(uint256[] calldata arr) public view returns (bool) {
         // your code here
+        // uint256 arr_lenght = arr.length;
+        for(uint256 i = 1; i < arr.length; i++){
+            console.log(i);
+            if (!(arr[i] >= arr[i-1])){
+                return false;
+            }
+            // else{
+            //     return true;
+            // }
+        }
+        return true;
     }
 }
--- Mean/src/Mean.sol
+++ Mean/src/Mean.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.13;
+import "forge-std/console.sol";
 
 contract Mean {
     /**
@@ -7,5 +8,11 @@ contract Mean {
      */
     function mean(uint256[] calldata arr) public view returns (uint256) {
         // your code here
+        uint256 total = 0;
+        for(uint256 i = 0; i < arr.length; i++){
+            total += arr[i];
+        }
+        console.log(total/arr.length);
+        return total/arr.length;
     }
 }
