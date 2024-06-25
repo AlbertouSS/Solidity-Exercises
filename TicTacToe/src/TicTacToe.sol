3c0c63c0e2b277967318899ee6cc3d50312f9e0f
AlbertouSS <soy_uno_2008@hotmail.com>
2024-06-24 19:45:30 -0600

    Excercise Solved

--- TicTacToe/src/TicTacToe.sol
+++ TicTacToe/src/TicTacToe.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.13;
+import "forge-std/console.sol";
 
 contract TicTacToe {
     /* 
@@ -12,5 +13,30 @@ contract TicTacToe {
 
     function isWinning(uint8[3][3] memory board) public pure returns (bool) {
         // your code here
+        // [1, 1, 0]                    [0, 1, 0]
+        // [0, 1, 0] -> isWinning       [0, 1, 0] -> isNotWinning
+        // [1, 0, 1]                    [1, 0, 1]
+
+        uint256 row1 = board[0][0]+board[0][1]+board[0][2];
+        uint256 row2 = board[1][0]+board[1][1]+board[1][2];
+        uint256 row3 = board[2][0]+board[2][1]+board[2][2];
+        uint256 col1 = board[0][0]+board[1][0]+board[2][0];
+        uint256 col2 = board[0][1]+board[1][1]+board[2][1];
+        uint256 col3 = board[0][2]+board[1][2]+board[2][2];
+        uint256 diag1 = board[0][0]+board[1][1]+board[2][2];
+        uint256 diag2 = board[0][2]+board[1][1]+board[2][0];
+
+        if((row1 == 3) || (row2 == 3) || (row3 == 3) || (col1 == 3) || (col2 == 3) || 
+        (col3 == 3) || (diag1 == 3) || (diag2 == 3)){
+            return true;
+        }
+        else{
+            return false;
+        }
+        // // console.log(row1, row2, row3);
+        // console.log(board[0][0], board[0][1], board[0][2]);
+        // console.log(board[1][0], board[1][1], board[1][2]);
+        // console.log(board[2][0], board[2][1], board[2][2]);
+        
     }
 }
